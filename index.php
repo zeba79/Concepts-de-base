d711232518248e30b4ad90630b74dc5e526ca4a5
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-09-18 12:21:43 +0200

    intégration sass

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/node_modules/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="scss/main.css">
     <title>POO Concept de base</title>
 </head>
 <body>

734da5e167f7a855515e03bec9a5b2b0f1075d56
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-09-18 17:16:35 +0200

    Organisation des pages

---
+++ footer.php
@@ -0,0 +1,22 @@
+<footer class="bg-dark text-white text-center footer" >
+    <div class="row">
+        <div class="col-12 col-lg-4" >
+            <h3 class="text-primary" >Nos horaires</h3>
+            <p>Du mardi au dimanche  12:00-14h00  18:00-23</p>
+        </div>
+        <div class="col-6 col-lg-4">
+            <p>Quai Antique<br/>
+            5 Bouvlevard de la Paix <br/>
+            69001 LYON <br/>
+            04 55 66 55 66
+            </p>
+        </div>
+        <div class="col-3 col-lg-4">
+            contact@quaiantique.com
+        </div>
+    </div>
+</footer>
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/scss/main.css">
+    <title>POO Concept de base</title>
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Quai Antique</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link"  href="/">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Galerie</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">La Carte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Les Réservation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mon Compte</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Connexion/Déconnexion</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+    </header>

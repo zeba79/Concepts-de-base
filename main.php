84610d81dadc3a61d41606bf78ee57fbe1714433
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-09-20 15:33:34 +0200

    ajout bloc Nos Menus

--- main.php
+++ main.php
@@ -8,5 +8,26 @@
         </div>
     </div>
 
-
 </main>
+<section>
+    <article>
+        <div class="container p-4">
+            <div>
+                <h2 class="text-center text-primary" >Bienvenue au Quai Antique</h2>
+            </div>
+            <div class="row row-cols-2 align-items-center p-4 " >
+                <div class="col">
+                    <p class="text-justify">
+                    Bienvenue au Quai Antique , le site de meilleur restaurateur de votre région,
+                    </p>
+                </div>
+                <div class="col">
+                    <img class="w-100 rounded" src="/img/gastronomy-2853387_640.jpg" alt="">
+                </div>
+            </div>
+            <div class="text-center p-4" >
+                <a href="#" class="btn btn-primary text-center" >Nos Menus</a>
+            </div>
+        </div>
+    </article>
+</section>

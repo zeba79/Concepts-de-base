08d48e4d357ef302009fff7f2b5826e99d80d8cf
Antoine ZEBAZE <zeba_antoine@yahoo.fr>
2024-09-20 16:00:48 +0200

    ajout card galarie et réservation

--- main.php
+++ main.php
@@ -8,26 +8,67 @@
         </div>
     </div>
 
+
+    <section>
+        <article>
+            <div class="container p-4">
+                <div>
+                    <h2 class="text-center text-primary" >Bienvenue au Quai Antique</h2>
+                </div>
+                <div class="row row-cols-2 align-items-center p-4 " >
+                    <div class="col">
+                        <p class="text-justify">
+                        Bienvenue au Quai Antique , le site de meilleur restaurateur de votre région,
+                        </p>
+                    </div>
+                    <div class="col">
+                        <img class="w-100 rounded" src="/img/salmon-sashimi-3637245_640.jpg" alt="">
+                    </div>
+                </div>
+                <div class="text-center p-4" >
+                    <a href="#" class="btn btn-primary text-center" >Nos Menus</a>
+                </div>
+            </div>
+        </article>
+        <article>
+            <div class="container p-4 bg-black text-white">
+                <div>
+                    <h2 class="text-center text-primary" >Des Produits frais</h2>
+                </div>
+                <div class="row row-cols-2 align-items-center p-4 " >
+                    <div class="col">
+                        <img class="w-100 rounded" src="/img/sashimi-1787626_640.jpg" alt="">
+                    </div>
+                    <div class="col">
+                        <p class="text-justify">
+                        Découvrez notre gamme de produits frais toute l’année
+                        </p>
+                    </div>
+                </div>
+                <div class="text-center p-4" >
+                    <a href="#" class="btn btn-primary text-center" >Voir galerie</a>
+                </div>
+            </div>
+        </article>
+        <article>
+            <div class="container p-4">
+                <div>
+                    <h2 class="text-center text-primary" >Réserver en Ligne</h2>
+                </div>
+                <div class="row row-cols-2 align-items-center p-4 " >
+                    <div class="col">
+                        <p class="text-justify">
+                        Réserver vos menus directement en ligne grâce à notre nouvelle plateforme
+                        </p>
+                    </div>
+                    <div class="col">
+                        <img class="w-100 rounded" src="/img/gastronomy-2853387_640.jpg" alt="">
+                    </div>
+                </div>
+                <div class="text-center p-4" >
+                    <a href="#" class="btn btn-primary text-center" >Réserver</a>
+                </div>
+            </div>
+        </article>
+    </section>
 </main>
-<section>
-    <article>
-        <div class="container p-4">
-            <div>
-                <h2 class="text-center text-primary" >Bienvenue au Quai Antique</h2>
-            </div>
-            <div class="row row-cols-2 align-items-center p-4 " >
-                <div class="col">
-                    <p class="text-justify">
-                    Bienvenue au Quai Antique , le site de meilleur restaurateur de votre région,
-                    </p>
-                </div>
-                <div class="col">
-                    <img class="w-100 rounded" src="/img/gastronomy-2853387_640.jpg" alt="">
-                </div>
-            </div>
-            <div class="text-center p-4" >
-                <a href="#" class="btn btn-primary text-center" >Nos Menus</a>
-            </div>
-        </div>
-    </article>
-</section>
